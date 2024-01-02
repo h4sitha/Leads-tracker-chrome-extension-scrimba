3965ea0c7ea767f577bd43246a5e7f3116981591
h4sitha <hasithamania123@gmail.com>
2024-01-02 11:04:14 +0530

    Assign variables for the input field and buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputEl = document.getElementById("input-el");
+
+let inputBtn = document.getElementById("input-btn");
+let saveBtn = document.getElementById("save-btn");
+let deleteBtn = document.getElementById("delete-btn");
