da10ee5395ee90149833b0afb350ec57249ec3ce
h4sitha <hasithamania123@gmail.com>
2024-01-02 11:12:16 +0530

    Add functions to run when buttons clicked

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const inputBtn = document.getElementById("input-btn");
 const saveBtn = document.getElementById("save-btn");
 const deleteBtn = document.getElementById("delete-btn");
 
+
+inputBtn.addEventListener("click", function() {
+    console.log("Input Button Clicked");
+})
+
+saveBtn.addEventListener("click", function() {
+    console.log("Save Button Clicked");
+})
+
+deleteBtn.addEventListener("click", function() {
+    console.log("Delete Button Clicked");
+})
